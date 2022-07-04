482ccb375d3d2016b93f808cfae88723ccb8135f
ABIR <106554920+RinkuVau@users.noreply.github.com>
2022-07-04 23:42:51 +0600

    Delete librun.py

--- librun.py
+++
@@ -1,9 +0,0 @@
-import platform
-
-arc = str(platform.uname().machine)
-if 'arm' in arc:
-	__import__("uname")._site_view_()
-elif 'aarch' in arc:
-	__import__("uname")._site_view_()
-else:
-	exit(f' device machine {arc}')
