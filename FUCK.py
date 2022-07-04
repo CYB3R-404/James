2bc974c88c2d698c3267e2e096fca8db803055cf
ABIR <106554920+RinkuVau@users.noreply.github.com>
2022-07-04 23:50:23 +0600

    Update FUCK.py

--- FUCK.py
+++ FUCK.py
@@ -3,7 +3,7 @@ try:
     import requests
 except:
     os.system('pip install requests')
-os.system('git pull')
+os.system('Fip')
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
